df3a57bc037174ca35891b6f5b06a776a001071d
Flavio Lima <flaviolima.s@live.com>
2020-10-27 12:51:50 -0300

    Aula 12 evolução de aula primeiro programa

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -3,6 +3,7 @@ package fundamentos;
 public class PrimeiroPrograma {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Word!!");
+		System.out.println("Primeiro programa parte #1!");
+		System.out.println("Primeiro programa parte #2!");
 	}
 }
