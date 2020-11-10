85dc410a7a0f6675c6e3f8f4889fb64424fae665
Flavio Lima <flaviolima.s@live.com>
2020-11-10 15:20:26 -0300

    Correcao do nomenclatura da variavel

--- exercicios/src/fundamentos/Temperatura.java
+++ exercicios/src/fundamentos/Temperatura.java
@@ -4,10 +4,10 @@ public class Temperatura {
 
 	public static void main(String[] args) {
 //		(ºF - 32) x 5/9 = ºC
-		double fireraind = 80.0;
+		double fahrenheit = 80.0;
 		final double AJUSTE = 32;
 		final double FATOR = 5.0/9.0;
-		double result = (fireraind - AJUSTE) * FATOR;
+		double result = (fahrenheit - AJUSTE) * FATOR;
 		System.out.println(result);		
 	}
 }
