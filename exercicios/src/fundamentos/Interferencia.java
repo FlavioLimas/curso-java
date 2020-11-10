ff48e338afe41101f9b5161b30bf8e92af31a89e
Flavio Lima <flaviolima.s@live.com>
2020-11-10 17:28:00 -0300

    Declarando variaveis sem o tipo no Java = Inferir a Variavel

--- exercicios/src/fundamentos/Interferencia.java
+++ exercicios/src/fundamentos/Interferencia.java
@@ -8,6 +8,12 @@ public class Interferencia {
 		
 		var b = 4.5;
 		System.out.println(b);
+		
+		var c = "Texto";
+		System.out.println(c);
+		
+		c = "Outro texto";
+		System.out.println(c);
 	}
 
 }
