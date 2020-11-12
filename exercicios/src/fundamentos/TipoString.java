55acc7981d325d94f6532fe76d84ecd73ca877c6
Flavio Lima <flaviolima.s@live.com>
2020-11-12 15:48:42 -0300

    Exemplo de concatenacao de strings

--- exercicios/src/fundamentos/TipoString.java
+++ exercicios/src/fundamentos/TipoString.java
@@ -4,6 +4,10 @@ public class TipoString {
 	
 	public static void main(String[] args) {
 		System.out.println("Olá pessoal".charAt(2));
+		
+		String s = "Boa tarde";
+		System.out.println(s.concat("!!!"));
+		System.out.println(s + "!!!");
 	}
 
 }
