55ed5b1951dafda25a2a27a5463490b883afb499
Flavio Lima <flaviolima.s@live.com>
2020-11-16 17:09:41 -0300

    Exemplos para utilizacao scanner no console

--- exercicios/src/fundamentos/Console.java
+++ exercicios/src/fundamentos/Console.java
@@ -1,5 +1,7 @@
 package fundamentos;
 
+import java.util.Scanner;
+
 public class Console {
 	public static void main(String[] args) {
 		System.out.print("Bom");
@@ -11,8 +13,23 @@ public class Console {
 		System.out.printf("Megasena: %d %d %d %d %d %d %n",
 				1, 2, 3, 4, 5, 6);
 		System.out.printf("Salário: %.1f%n", 1234.5678);
-		System.out.printf("Nome: %s", "Flavio Lima");
+		System.out.printf("Nome: %s", "Flavio Lima\n");
 		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite o seu nome: ");
+		String nome = entrada.nextLine();
+		
+		System.out.print("Digite o seu sobrenome: ");
+		String sobrenome = entrada.nextLine();
+		
+		System.out.print("Digite sua idade: ");
+		int idade = entrada.nextInt();
+		
+		System.out.printf("%s %s tem %d anos.%n",
+				nome, sobrenome, idade);
+		
+		entrada.close();
 	}
 
 }
