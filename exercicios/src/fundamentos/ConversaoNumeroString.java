82fdc447ca87eb949e08d04835212e79c82c6d23
Flavio Lima <flaviolima.s@live.com>
2021-01-18 16:29:17 -0300

    Convesao numerica para string

--- exercicios/src/fundamentos/ConversaoNumeroString.java
+++ exercicios/src/fundamentos/ConversaoNumeroString.java
@@ -7,6 +7,9 @@ public class ConversaoNumeroString {
 		Integer num1 = 10000;
 		System.out.println(num1.toString().length());
 		
+		int num2 = 100000;
+		System.out.println(Integer.toString(num2).length());
+		
 	}
 
 }
