0f27547e8378769ca428337e53ff52276bfc0e0e
Flavio Lima <flaviolima.s@live.com>
2021-01-18 15:47:29 -0300

    Convesao implicita e explicita

---
+++ exercicios/src/fundamentos/ConversaoTipoPrimitivosNumerico.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class ConversaoTipoPrimitivosNumerico {
+
+	public static void main(String[] args) {
+
+		double a = 1; // implicita
+		System.out.println(a);
+		
+		float b = (float) 1.2345888888888; // explicta (CAST)
+		
+		System.out.println(b);
+		
+	}
+
+}
