7ddbf2ccd13b2be08d0474dd40776277bca2bed9
Flavio Lima <flaviolima.s@live.com>
2021-02-04 19:59:34 -0300

    Tratando os caracteres informados para minusculos

--- exercicios/src/controle/DesafioDiasSemana.java
+++ exercicios/src/controle/DesafioDiasSemana.java
@@ -7,19 +7,19 @@ public class DesafioDiasSemana {
         System.out.print("Informe do dia da semana: ");
         String diaSemana = entrada.next();
         
-        if (diaSemana.equals("Domingo")) {
+        if (diaSemana.toLowerCase().equals("domingo")) {
             System.out.println(1);
-        } else if (diaSemana.equals("Segunda")) {
+        } else if (diaSemana.equals("segunda")) {
             System.out.println(2);
-        } else if (diaSemana.equals("Terca")) {
+        } else if (diaSemana.equals("terca")) {
             System.out.println(3);
-        } else if (diaSemana.equals("Quarta")) {
+        } else if (diaSemana.equals("quarta")) {
             System.out.println(4);
-        } else if (diaSemana.equals("Quinta")) {
+        } else if (diaSemana.equals("quinta")) {
             System.out.println(5);
-        } else if (diaSemana.equals("Sexta")) {
+        } else if (diaSemana.equals("sexta")) {
             System.out.println(6);
-        } else if (diaSemana.equals("Sabado")) {
+        } else if (diaSemana.equals("sabado")) {
             System.out.println(7);
         } else {
             System.out.print("Infomrado dia inválido");
