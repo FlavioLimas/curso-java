b1961d37ab1ee38347896540a572700366e5ddf0
Flavio Lima <flaviolima.s@live.com>
2021-02-05 12:56:02 -0300

    Implementado versao a partir da correcao

--- exercicios/src/controle/DesafioWhile.java
+++ exercicios/src/controle/DesafioWhile.java
@@ -5,28 +5,27 @@ public class DesafioWhile {
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
+        
+        int count = 0;
         double nota = 0;
-        double count = 0;
-        while (nota != -1) {
-            
-            
-            if (nota >= 0 || nota <= 10) {
-                System.out.println("Favor informar a nota: ");
-                nota = Double.parseDouble(entrada.next());
-                
-                count++;
-                if (count > 1) {
-                    
-                    nota =+ nota;
-                    System.out.println("A mádia é: " + nota / count); 
+        double total = 0;
 
-                }
-                
-            } else {
-                System.out.println("Favor infomrar uma nota valida!");
-                nota = -1;
-            }
+        while (nota != -1) {
+        System.out.println("Favor informar a nota: ");
+        nota = Double.parseDouble(entrada.next());
+            
+        if (nota >= 0 && nota <= 10) {
+            
+            total += nota;
+            count++;
+
+        } else if(nota != -1) {
+            System.out.println("Favor infomrar uma nota valida!");
+            nota = -1;
         }
+        }
+        double media = total / count;
+        System.out.println("Média = " + media); 
         entrada.close();
     }
 }
