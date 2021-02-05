a23437d44d7e1080065fc4ffd068863451a82ae2
Flavio Lima <flaviolima.s@live.com>
2021-02-05 18:06:29 -0300

    Resultado do desafio

---
+++ exercicios/src/controle/DesafioFor.java
@@ -0,0 +1,8 @@
+public class DesafioFor {
+    public static void main(String[] args) {
+                
+        for (String i = "#"; !i.equals("######"); i += "#") {
+            System.out.println(i);
+        }
+    }
+}
