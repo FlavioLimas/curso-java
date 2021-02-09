36dd58c73d5ed8da3f064ae01ae58854c92ca378
Flavio Lima <flaviolima.s@live.com>
2021-02-09 20:52:50 -0300

    Definindo metodos com assinaturas diferentes

--- exercicios/src/classe/ProdutoTeste.java
+++ exercicios/src/classe/ProdutoTeste.java
@@ -13,8 +13,8 @@ public class ProdutoTeste {
         System.out.println(p1.nome);
         System.out.println(p2.nome);
 
-        double precoFinal1 = p1.preco * (1 - p1.desconto);
-        double precoFinal2 = p2.preco * (1 - p2.desconto);
+        double precoFinal1 = p1.precoComDesconto(); // Sem desconto do gerente
+        double precoFinal2 = p2.precoComDesconto(0.1); // Com 10 % de desconto do gerente
         double mediaCarrinho = (precoFinal1 + precoFinal2) / 2;
 
         System.out.printf("Média do carrinho = R$%.2f.", mediaCarrinho);
