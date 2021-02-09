5a7c4a2fd67e5964370726abd603837c45650d21
Flavio Lima <flaviolima.s@live.com>
2021-02-09 20:35:21 -0300

    Implementacao da correcao do desafio

--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -11,19 +11,7 @@ public class DataTeste {
         data2.mes = 1;
         data2.ano = 2021;
 
-        String niver = Integer.toString(data1.dia);
-        niver += "/";
-        niver += Integer.toString(data1.mes);
-        niver += "/";
-        niver += Integer.toString(data1.ano);
-
-        String anoNovo = Integer.toString(data2.dia);
-        anoNovo += "/";
-        anoNovo += Integer.toString(data2.mes);
-        anoNovo += "/";
-        anoNovo += Integer.toString(data2.ano);
-
-        System.out.println("Meu Aniversário " + niver);
-        System.out.println("Ano novo " + anoNovo);
+        System.out.printf("%d/%d/%d\n", data1.dia, data1.mes, data1.ano);
+        System.out.printf("%d/%d/%d", data2.dia, data2.mes, data2.ano);
     }
 }
