57a89c8dda6c06d0968e2b244829f252b295beb8
Flavio Lima <flaviolima.s@live.com>
2021-02-10 20:01:56 -0300

    Implementacao da resposta do desafio com sucesso

--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -1,15 +1,9 @@
 public class DataTeste {
     public static void main(String[] args) {
         
-        Data data1 = new Data();
-        data1.dia = 25;
-        data1.mes = 11;
-        data1.ano = 1985;
+        Data data1 = new Data(25, 11, 1985);
 
         Data data2 = new Data();
-        data2.dia = 1;
-        data2.mes = 1;
-        data2.ano = 2021;
 
         System.out.println(data1.obterDataFormatada());
         System.out.println(data2.obterDataFormatada());
