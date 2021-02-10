b37d635cf00dfeeb646770033fa9b8231f7efe7d
Flavio Lima <flaviolima.s@live.com>
2021-02-10 16:32:07 -0300

    Exemplo de contrutores

--- exercicios/src/classe/Produto.java
+++ exercicios/src/classe/Produto.java
@@ -3,7 +3,20 @@ public class Produto {
     String nome;
     double preco;
     double desconto;
-    
+
+    Produto() {
+
+    }
+        
+    Produto(String nomeInicial) {
+        nome = nomeInicial;
+    }
+
+    Produto(String nomeInicial, double precoInicial) {
+        nome = nomeInicial;
+        preco = precoInicial;
+    }
+
     double precoComDesconto() {
         return preco * (1 - desconto);
     }
