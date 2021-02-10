a443f28bb237a54bc76c743a6ab25a31b042b460
Flavio Lima <flaviolima.s@live.com>
2021-02-10 15:55:45 -0300

    Exemplificando que uma funcao pode chamar a outra

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -4,11 +4,12 @@ public class Data {
     int mes;
     int ano;
 
-    String obterDataFormatada(int dia, int mes, int ano) {
-        String diaS = (dia < 10 ? ("0" + Integer.toString(dia) + "/") : (Integer.toString(dia) + "/"));
-        String mesS = (mes < 10 ? ("0" + Integer.toString(mes) + "/") : (Integer.toString(mes) + "/"));
-        String data = (diaS + mesS + Integer.toString(ano));
-        return data;
+    String obterDataFormatada() {
+        return String.format("%d/%d/%d", dia, mes, ano);
+    }
+
+    void imprimirDataFormatada() {
+        System.out.println(obterDataFormatada());
     }
     
 }
--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -11,7 +11,10 @@ public class DataTeste {
         data2.mes = 1;
         data2.ano = 2021;
 
-        System.out.println(data1.obterDataFormatada(data1.dia, data1.mes, data1.ano));
-        System.out.println(data2.obterDataFormatada(data2.dia, data2.mes, data2.ano));
+        System.out.println(data1.obterDataFormatada());
+        System.out.println(data2.obterDataFormatada());
+
+        data1.imprimirDataFormatada();
+        data2.imprimirDataFormatada();
     }
 }
