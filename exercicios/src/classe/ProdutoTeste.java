953cf936f3892b71e2d7bc1d488d667c71102d2e
Flavio Lima <flaviolima.s@live.com>
2021-02-10 21:09:49 -0300

    Correcao do desafio de implementacao de static

--- exercicios/src/classe/ProdutoTeste.java
+++ exercicios/src/classe/ProdutoTeste.java
@@ -1,14 +1,15 @@
 public class ProdutoTeste {
     public static void main(String[] args) {
-        Produto p1 = new Produto("Notebook", 4356.89, 0.25);
+        Produto p1 = new Produto("Notebook", 4356.89);
 
         var p2 = new Produto();
         p2.nome = "Caneta Preta";
         p2.preco = 12.56;
-        p2.desconto = 0.29;
         
-        System.out.println(p1.nome);
-        System.out.println(p2.nome);
+        Produto.desconto = 0.29;
+        
+        System.out.println(p1.nome + " " + p1.precoComDesconto());
+        System.out.println(p2.nome + " " + p2.precoComDesconto());
 
         double precoFinal1 = p1.precoComDesconto(); // Sem desconto do gerente
         double precoFinal2 = p2.precoComDesconto(0.1); // Com 10 % de desconto do gerente
