ff16f02b28a228035a7ee6b453e7599e7526c07b
Flavio Lima <flaviolima.s@live.com>
2021-02-11 22:57:45 -0300

    Ajustando conforme a correcao

--- exercicios/src/classe/Desafio/Comida.java
+++ exercicios/src/classe/Desafio/Comida.java
@@ -4,10 +4,6 @@ public class Comida {
     String nome;
     double peso;
 
-    Comida() {
-        
-    }
-
     Comida(String nome, double peso) {
         this.nome = nome;
         this.peso = peso;
--- exercicios/src/classe/Desafio/Jantar.java
+++ exercicios/src/classe/Desafio/Jantar.java
@@ -2,18 +2,14 @@ package Desafio;
 
 public class Jantar {
     public static void main(String[] args) {
-        Comida comida1 = new Comida();
-        Comida comida2 = new Comida();
+        Comida comida1 = new Comida("feijão", 0.225);
+        Comida comida2 = new Comida("arroz", 0.100);
         
         Pessoa pessoa1 = new Pessoa("Flavio", 50.);
         Pessoa pessoa2 = new Pessoa("Fulano", 70.);
 
         System.out.printf("Antes da refeicao peso %.3f do %s\n", pessoa2.peso, pessoa2.nome);
         System.out.printf("Antes da refeicao peso %.3f do %s\n", pessoa1.peso, pessoa1.nome);
-        comida1.nome = "feijão";
-        comida1.peso = 0.225;
-        comida2.nome = "arroz";
-        comida2.peso = 0.100;
         
         pessoa1.comer(comida1);
         pessoa2.comer(comida2);
