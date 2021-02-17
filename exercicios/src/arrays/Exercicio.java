972c13c0b094b92d953f16315ac2c96fa4bbbf05
Flavio Lima <flaviolima.s@live.com>
2021-02-17 19:16:17 -0300

    Calculando media de notas no Array

--- exercicios/src/arrays/Exercicio.java
+++ exercicios/src/arrays/Exercicio.java
@@ -11,6 +11,15 @@ public class Exercicio {
         notasAlunoA[2] = 6.7;
 
         System.out.println(Arrays.toString(notasAlunoA));
+
+        // Calculando media do aluno
+
+        double total = 0;
+        for(int i = 0; i < notasAlunoA.length; i++) {
+            total += notasAlunoA[i];
+        }
+        // Na operação entre um double e um int o resultado será sempre um double
+        System.out.println(total / notasAlunoA.length);
     }    
 
 }
