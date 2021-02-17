84e7b6c2ef8d183232ad1104722977bc8bd57fda
Flavio Lima <flaviolima.s@live.com>
2021-02-17 19:18:47 -0300

    Usanto metodo Array.length

--- exercicios/src/arrays/Exercicio.java
+++ exercicios/src/arrays/Exercicio.java
@@ -3,12 +3,13 @@ import java.util.Arrays;
 public class Exercicio {
     public static void main(String[] args) {
         
-        double[] notasAlunoA = new double[3];
+        double[] notasAlunoA = new double[4];
         System.out.println(Arrays.toString(notasAlunoA));
 
         notasAlunoA[0] = 7.9;
         notasAlunoA[1] = 8;
         notasAlunoA[2] = 6.7;
+        notasAlunoA[3] = 9.7;
 
         System.out.println(Arrays.toString(notasAlunoA));
 
