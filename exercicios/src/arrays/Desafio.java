3edfdbc4102e32af4beda18f0061c9c5b4e3204d
Flavio Lima <flaviolima.s@live.com>
2021-02-17 21:28:43 -0300

    Correcao do desafio

--- exercicios/src/arrays/Desafio.java
+++ exercicios/src/arrays/Desafio.java
@@ -1,18 +1,20 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Desafio {
     public static void main(String[] args) {
-        System.out.println("Qunatas notas voce gostaria de informar: ");
+        System.out.println("Quantas notas: ");
         Scanner entrada = new Scanner(System.in);
 
         int posicoes =  entrada.nextInt();
         double notas[] = new double[posicoes];
 
         for (int i = 0; i < notas.length; i++) {
-            System.out.print("Informe a nota: ");
+            System.out.print("Informe a nota " + (i + 1) + ": ");
+            // Tem que ser informado o valor com "," e não "." para os campos decimais devido a configuração do S.O. em pt-br
             notas[i] = entrada.nextDouble();
-            System.out.println("Nota informada " + notas[i]);
         }
+        System.out.println(Arrays.toString(notas));
         entrada.close();
 
         double soma = 0;
