210660949a3546c31365170fe7f6e1f499f30950
Flavio Lima <flaviolima.s@live.com>
2021-02-18 21:25:29 -0300

    Implementado sobrescrita do metodo equals()

--- exercicios/src/classe/Usuario.java
+++ exercicios/src/classe/Usuario.java
@@ -4,11 +4,9 @@ public class Usuario {
 
     public boolean equals(Object objeto) {
 
-        if (objeto instanceof Usuario) {
-            Usuario outro = (Usuario) objeto;
-            return super.equals(objeto);
-        } else {
-            return false;
-        }
+        Usuario outro = (Usuario) objeto;
+        boolean nomeIgual = outro.nome.equals(this.nome);
+        boolean emailIgual = outro.email.equals(this.email);
+        return nomeIgual && emailIgual;
     }
 }
