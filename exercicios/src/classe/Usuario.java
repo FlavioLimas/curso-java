b708adfa8ce5ea650aff781bfd90597abf10d1b6
Flavio Lima <flaviolima.s@live.com>
2021-02-18 21:09:43 -0300

    Sobrescrevendo metodo equals com validacao de estancia de Usuario

--- exercicios/src/classe/Usuario.java
+++ exercicios/src/classe/Usuario.java
@@ -1,4 +1,14 @@
 public class Usuario {
     String nome;
     String email;
+
+    public boolean equals(Object objeto) {
+
+        if (objeto instanceof Usuario) {
+            Usuario outro = (Usuario) objeto;
+            return super.equals(objeto);
+        } else {
+            return false;
+        }
+    }
 }
