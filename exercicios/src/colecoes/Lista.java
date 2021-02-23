032a531ea1a0af3575e729ecd51af148368424c5
Flavio Lima <flaviolima.s@live.com>
2021-02-22 22:08:05 -0300

    Implementando uma lista

---
+++ exercicios/src/colecoes/Lista.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Lista {
+    public static void main(String[] args) {
+        ArrayList<Usuarios> list = new ArrayList<>();
+
+        Usuarios u1 = new Usuarios("Ana");
+        list.add(u1);
+        list.add(new Usuarios("Carlos"));
+        list.add(new Usuarios("Lia"));
+        list.add(new Usuarios("Bia"));
+        list.add(new Usuarios("Manu"));
+
+        
+
+        for (Usuarios u : list) {
+            System.out.println(u.nome);
+        }
+    }
+}
