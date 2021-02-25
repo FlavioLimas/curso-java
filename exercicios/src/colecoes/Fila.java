9ac3b8b12fbd6b38e0b57a6e0ca353f16b91fa58
Flavio Lima <flaviolima.s@live.com>
2021-02-24 21:18:30 -0300

    Caso a fila vazia dispara uma exception no element

--- exercicios/src/colecoes/Fila.java
+++ exercicios/src/colecoes/Fila.java
@@ -7,15 +7,19 @@ public class Fila {
 
         // Offer e Add -> adicionam elementos na fila
         //Diferença é o comportamento quando a fila está cheia!
-        fila.add("Ana");
-        fila.offer("Bia");
-        fila.add("Carlos");
-        fila.offer("Daniel");
-        fila.add("Rafaela");
-        fila.offer("Gui");
+        // fila.add("Ana");
+        // fila.offer("Bia");
+        // fila.add("Carlos");
+        // fila.offer("Daniel");
+        // fila.add("Rafaela");
+        // fila.offer("Gui");
 
+        // Lista vazia peek retorna null
+        // Lista vazia element dispara uma excepition java.util.NoSuchElementException
+        
         System.out.println(fila.peek());
         System.out.println(fila.peek());
-        // System.out.println(fila.element());
+        System.out.println(fila.element());
+        System.out.println(fila.element());
     }
 }
