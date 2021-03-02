4d8de15d7446e944d3e813ba5a096d4aee656cbf
Flavio Lima <flaviolima.s@live.com>
2021-03-01 21:39:48 -0300

    ligando carro

--- exercicios/src/classe/oo/composicao/CarroTeste.java
+++ exercicios/src/classe/oo/composicao/CarroTeste.java
@@ -4,5 +4,9 @@ public class CarroTeste {
     public static void main(String[] args) {
         Carro c1 = new Carro();
         System.out.println(c1.estaLigado());
+
+        c1.ligar();
+
+      
     }
 }
