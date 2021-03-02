381071847548dcc0265af30d0cb3f01c82c2e6e0
Flavio Lima <flaviolima.s@live.com>
2021-03-02 19:20:25 -0300

    Abstracao

--- exercicios/src/classe/oo/composicao/CompraTeste.java
+++ exercicios/src/classe/oo/composicao/CompraTeste.java
@@ -5,7 +5,7 @@ public class CompraTeste {
         
         Compra compra1 = new Compra();
         compra1.cliente = "João Pedro";
-        compra1.addItemAndAssociaCompra(new Item("Caneta", 20, 7.45));
+        compra1.addItemAndAssociaCompra("Caneta", 20, 7.45);
         compra1.addItemAndAssociaCompra(new Item("Borracha", 12, 3.89));
         compra1.addItemAndAssociaCompra(new Item("Caderno", 3, 18.79));
 
