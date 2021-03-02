6ded25f26d7c12be665f46463f5ba16948f63bdd
Flavio Lima <flaviolima.s@live.com>
2021-03-02 15:45:59 -0300

    Implementando classes Compra e Item

---
+++ exercicios/src/classe/oo/composicao/CompraTeste.java
@@ -0,0 +1,17 @@
+package oo.composicao;
+
+public class CompraTeste {
+    public static void main(String[] args) {
+        Compra compra = new Compra();
+        Item item = new Item();
+        compra.cliente = "João";
+        item.nome = "Bola";
+        item.quantidade = 1;
+        item.preco = 5;
+
+        compra.itens.add(item);
+
+        System.out.println("O " + compra.cliente + " Comprou " + item.quantidade + " " + item.nome + " pelo valor R$ "
+                + String.format("%.2f", item.preco));
+    }
+}
