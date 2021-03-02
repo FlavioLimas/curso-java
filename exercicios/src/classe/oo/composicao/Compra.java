d0374b4c1401fac50cf9bc84ecd9712660613983
Flavio Lima <flaviolima.s@live.com>
2021-03-02 18:57:24 -0300

    Implementando getValorToral da compra

--- exercicios/src/classe/oo/composicao/Compra.java
+++ exercicios/src/classe/oo/composicao/Compra.java
@@ -5,4 +5,14 @@ import java.util.ArrayList;
 public class Compra {
     String cliente;    
     ArrayList<Item> itens = new ArrayList<Item>();
+
+    double getValorTotal() {
+        double total = 0;
+
+        for (Item item : itens) {
+            total += item.quantidade * item.preco;
+        }
+
+        return total;
+    }
 }
