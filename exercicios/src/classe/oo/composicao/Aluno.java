27cd2be2e2dc26d6141c1a3307d89cc7dccb51f8
Flavio Lima <flaviolima.s@live.com>
2021-03-02 22:49:59 -0300

    Implementando pesquisa por nome do curso

--- exercicios/src/classe/oo/composicao/Aluno.java
+++ exercicios/src/classe/oo/composicao/Aluno.java
@@ -16,6 +16,15 @@ public class Aluno {
         curso.alunos.add(this); // Adicionando esse curso ao aluno recebido como parametro
     }
 
+    Curso getCursoPorNome(String nome) {
+        for (Curso curso : this.cursos) {
+            if (curso.nome.equalsIgnoreCase(nome)) {
+                return curso;
+            }
+        }
+        return null;
+    }
+
     // Sempre que for necessario converter um objeto em string deve ser implementado o metodo toString
     public String toString() {
         return nome;
