3a04632c2df8bb814828c239ff14e5397c824f22
Flavio Lima <flaviolima.s@live.com>
2021-03-03 15:43:45 -0300

    Implementado Classes do desafio proposto

--- exercicios/src/classe/oo/composicao/desafio/Cliente.java
+++ exercicios/src/classe/oo/composicao/desafio/Cliente.java
@@ -1,5 +1,17 @@
 package oo.composicao.desafio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
-    
+    String nome;
+    List<Compra> compras = new ArrayList<Compra>();
+
+    double getValorTotal() {
+        double valorToral = .0;
+        for (Compra compra : compras) {
+            valorToral += compra.getValorToral();
+        }
+        return valorToral;
+    }
 }
--- exercicios/src/classe/oo/composicao/desafio/Compra.java
+++ exercicios/src/classe/oo/composicao/desafio/Compra.java
@@ -1,5 +1,16 @@
 package oo.composicao.desafio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Compra {
-    
+    List<Item> itens = new ArrayList<Item>();
+
+    double getValorToral() {
+        double valorTotal = .0;
+        for(Item item : itens) {
+            valorTotal += (item.quantidade * item.produto.preco);
+        }
+        return valorTotal;
+    }
 }
