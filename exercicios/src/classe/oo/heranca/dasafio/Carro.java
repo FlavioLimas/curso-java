19bdf4495bf7df970359460690c7d607ce49b88a
Flavio Lima <flaviolima.s@live.com>
2021-03-08 20:35:14 -0300

    Alteracao na assinatura dos metodos

--- exercicios/src/classe/oo/heranca/dasafio/Carro.java
+++ exercicios/src/classe/oo/heranca/dasafio/Carro.java
@@ -3,7 +3,13 @@ package oo.heranca.dasafio;
 public class Carro {
     int velocidadeAtual;
 
-    void acelerar(int velocidadeAtual) {
-        velocidadeAtual += 5;
+    void acelerar() {
+        this.velocidadeAtual += 5;
+    }
+
+    void frear() {
+        if (this.velocidadeAtual > 0) {
+            this.velocidadeAtual -= 5;
+        }
     }
 }
