f3a47e47ce173beb747cb44a97c79d556e7c110f
Flavio Lima <flaviolima.s@live.com>
2021-03-08 20:42:07 -0300

    Implementacao do acelerar na ferrari

--- exercicios/src/classe/oo/heranca/dasafio/Ferrari.java
+++ exercicios/src/classe/oo/heranca/dasafio/Ferrari.java
@@ -1,5 +1,9 @@
 package oo.heranca.dasafio;
 
 public class Ferrari extends Carro {
-    
+    void acelerar() {
+        super.acelerar();
+        super.acelerar();
+        super.acelerar();
+    }
 }
