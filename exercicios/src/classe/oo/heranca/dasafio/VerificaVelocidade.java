d7c406ca8fee1e6627d1bd62e1da32f104890d8d
Flavio Lima <flaviolima.s@live.com>
2021-03-08 21:06:37 -0300

    Implementacao do resultado do desafio

--- exercicios/src/classe/oo/heranca/dasafio/VerificaVelocidade.java
+++ exercicios/src/classe/oo/heranca/dasafio/VerificaVelocidade.java
@@ -3,7 +3,34 @@ package oo.heranca.dasafio;
 public class VerificaVelocidade {
     public static void main(String[] args) {
         Civic civic = new Civic();
+        Ferrari ferrari = new Ferrari();
 
-        System.out.println(civic);
+        System.out.println("Velocidade atual Civic >>> " + civic);
+        System.out.println("Velocidade atual Ferrari >>> " + ferrari);
+
+        civic.acelerar();
+        ferrari.acelerar();
+        civic.acelerar();
+        ferrari.acelerar();
+        civic.acelerar();
+        ferrari.acelerar();
+
+        civic.frear();
+        ferrari.frear();
+        civic.frear();
+        ferrari.frear();
+        civic.frear();
+        ferrari.frear();
+        civic.frear();
+        ferrari.frear();
+        civic.frear();
+        ferrari.frear();
+        ferrari.frear();
+        ferrari.frear();
+        ferrari.frear();
+        ferrari.frear();
+        
+        System.out.println("Velocidade atual Civic >>> " + civic);
+        System.out.println("Velocidade atual Ferrari >>> " + ferrari);
     }
 }
