d673415f5f1d80e7dfe4a97a0c6be765aba22446
Flavio Lima <flaviolima.s@live.com>
2021-03-08 21:01:08 -0300

    Implementando Classe para verificar velocidade

---
+++ exercicios/src/classe/oo/heranca/dasafio/VerificaVelocidade.java
@@ -0,0 +1,9 @@
+package oo.heranca.dasafio;
+
+public class VerificaVelocidade {
+    public static void main(String[] args) {
+        Civic civic = new Civic();
+
+        System.out.println(civic);
+    }
+}
