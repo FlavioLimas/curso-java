902af5d35a34087ec61911fda810909786227e6a
Flavio Lima <flaviolima.s@live.com>
2021-03-11 20:52:32 -0300

    Niveis de acesso dos atributos

--- exercicios/src/classe/oo/encapsulamento/casaA/Paulo.java
+++ exercicios/src/classe/oo/encapsulamento/casaA/Paulo.java
@@ -1,5 +1,13 @@
 package oo.encapsulamento.casaA;
 
 public class Paulo {
-    
+    Ana esposa = new Ana();
+
+    void testeAcessos() {
+        
+        System.out.println(esposa.segredo); // Atributo privado na Classe Ana
+        System.out.println(esposa.facoDentroDeCasa);
+        System.out.println(esposa.formaDeFalar);
+        System.out.println(esposa.todosSabem);
+    }
 }
