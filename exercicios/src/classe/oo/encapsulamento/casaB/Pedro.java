7f740d809c2ba94ac2dbf1f885e21bd64c773546
Flavio Lima <flaviolima.s@live.com>
2021-03-11 21:55:15 -0300

    Acessando atributos por herança

--- exercicios/src/classe/oo/encapsulamento/casaB/Pedro.java
+++ exercicios/src/classe/oo/encapsulamento/casaB/Pedro.java
@@ -1,5 +1,15 @@
 package oo.encapsulamento.casaB;
 
-public class Pedro {
-    
+import oo.encapsulamento.casaA.Ana;
+
+public class Pedro extends Ana {
+
+    void testeAcessos() {
+        
+        // System.out.println(mae.segredo);
+        // System.out.println(facoDentroDeCasa);
+        System.out.println(formaDeFalar); // Acessando por herança
+        System.out.println(todosSabem); // Acessando por herança
+    }
+
 }
