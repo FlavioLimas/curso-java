f260dd00c6b845bdcd569275e3015afefc85c081
Flavio Lima <flaviolima.s@live.com>
2021-03-11 22:17:00 -0300

    Retirando erro

--- exercicios/src/classe/oo/encapsulamento/casaA/Paulo.java
+++ exercicios/src/classe/oo/encapsulamento/casaA/Paulo.java
@@ -5,7 +5,7 @@ public class Paulo {
 
     void testeAcessos() {
         
-        System.out.println(esposa.segredo); // Atributo privado na Classe Ana
+        // System.out.println(esposa.segredo); // Atributo privado na Classe Ana
         System.out.println(esposa.facoDentroDeCasa);
         System.out.println(esposa.formaDeFalar);
         System.out.println(esposa.todosSabem);
