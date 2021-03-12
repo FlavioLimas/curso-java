a0f0e98cf02c22e94f523fed0e522d3afeb1a58b
Flavio Lima <flaviolima.s@live.com>
2021-03-11 22:13:38 -0300

    Modelo de aula

--- exercicios/src/classe/oo/encapsulamento/casaB/Julia.java
+++ exercicios/src/classe/oo/encapsulamento/casaB/Julia.java
@@ -1,5 +1,17 @@
 package oo.encapsulamento.casaB;
 
+import oo.encapsulamento.casaA.Ana;
+
 public class Julia {
+        
+    Ana sogra = new Ana();
+
+    void testeAcessos() {
+        
+        // System.out.println(sogra.segredo); // Atributo privado na Classe Ana
+        // System.out.println(sogra.facoDentroDeCasa);
+        // System.out.println(sogra.formaDeFalar);
+        System.out.println(sogra.todosSabem);
+    }
     
 }
