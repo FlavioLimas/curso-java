bf82908d35ddb0f9c8c681394d63a9a3d0ebf4e9
Flavio Lima <flaviolima.s@live.com>
2021-03-15 21:29:41 -0300

    Implementacao do set idade

--- exercicios/src/classe/oo/encapsulamento/PessoaTeste.java
+++ exercicios/src/classe/oo/encapsulamento/PessoaTeste.java
@@ -4,7 +4,7 @@ public class PessoaTeste {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa(-30);
 
-        // p1.setIdade(-30);;
+        p1.setIdade(40);;
 
         System.out.println(p1.getIdade());
     }
