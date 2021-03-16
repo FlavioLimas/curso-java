6c8930416f46102c4c5292cb253edc7ffb1448d0
Flavio Lima <flaviolima.s@live.com>
2021-03-15 21:25:31 -0300

    Implementacao recurso para ignorar num negativo

--- exercicios/src/classe/oo/encapsulamento/Pessoa.java
+++ exercicios/src/classe/oo/encapsulamento/Pessoa.java
@@ -4,13 +4,13 @@ public class Pessoa {
     private int idade;
 
     public Pessoa(int idade) {
-        this.idade = idade;
+        this.idade = Math.abs(idade);
     }
     public int getIdade() {
         return this.idade;
     }
 
     public void setIdade(int idade) {
-        this.idade = idade;
+        this.idade = Math.abs(idade);
     }
 }
