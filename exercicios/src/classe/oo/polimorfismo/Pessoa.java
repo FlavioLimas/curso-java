f97a3134740395fed3d9497f0a8fe84b632de639
Flavio Lima <flaviolima.s@live.com>
2021-03-16 22:55:39 -0300

    Implementacao das respactivas classes

--- exercicios/src/classe/oo/polimorfismo/Pessoa.java
+++ exercicios/src/classe/oo/polimorfismo/Pessoa.java
@@ -6,7 +6,15 @@ public class Pessoa {
     public void comer(Arroz arroz) {
         this.peso += arroz.getPeso();
     }
-    
+
+    public void comer(Feijao feijao) {
+        this.peso += feijao.getPeso();
+    }
+
+    public void comer(Sorvete sorvete) {
+        this.peso += sorvete.getPeso();
+    }
+
     public Pessoa(double peso) {
         setPeso(peso);
     }
