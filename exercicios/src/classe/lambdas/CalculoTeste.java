bdbf4ace59ed61c9ad89e930874ba5ca9f24343e
Flavio Lima <flaviolima.s@live.com>
2021-03-18 18:06:50 -0300

    Sobrescrevendo valor da variavel com nova instancia

--- exercicios/src/classe/lambdas/CalculoTeste.java
+++ exercicios/src/classe/lambdas/CalculoTeste.java
@@ -5,9 +5,9 @@ public class CalculoTeste {
     public static void main(String[] args) {
         
         Calculo calculo = new Somar();
-        Calculo multiplicar = new Multiplicar();
-
         System.out.println(calculo.executar(2, 3));
+
+        calculo = new Multiplicar();
     }
     
 }
