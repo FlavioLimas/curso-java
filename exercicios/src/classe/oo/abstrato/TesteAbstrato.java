097c79b80f27386233c8c88688fef807b2d43e52
Flavio Lima <flaviolima.s@live.com>
2021-03-17 23:32:28 -0300

    Instanciando cachorro e implementando os metodos

--- exercicios/src/classe/oo/abstrato/TesteAbstrato.java
+++ exercicios/src/classe/oo/abstrato/TesteAbstrato.java
@@ -2,6 +2,8 @@ package oo.abstrato;
 
 public class TesteAbstrato {
     public static void main(String[] args) {
-        
+        Animal a = new Cachorro();
+        System.out.println(a.mover());
+        System.out.println(a.respirar());
     }
 }
