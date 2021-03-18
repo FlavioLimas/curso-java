e53f372d35ec1c8b7a9671c1db65ad209378a392
Flavio Lima <flaviolima.s@live.com>
2021-03-17 23:11:56 -0300

    exemplo de implementacao de classe abstract

--- exercicios/src/classe/oo/polimorfismo/Jantar.java
+++ exercicios/src/classe/oo/polimorfismo/Jantar.java
@@ -7,11 +7,14 @@ public class Jantar {
         Arroz ingrediente1 = new Arroz(0.2);
         Feijao ingrediente2 = new Feijao(0.1);
 
+        Alimento ingrediente3 = new Arroz(.3);
+
         System.out.println(convidado.getPeso());
 
         convidado.comer(ingrediente1);
-        convidado.comer(ingrediente2);
-
+        convidado.comer(ingrediente2); 
+        convidado.comer(ingrediente3); 
+        
         System.out.println(convidado.getPeso());
 
         Sorvete sobremesa = new Sorvete(0.4);
