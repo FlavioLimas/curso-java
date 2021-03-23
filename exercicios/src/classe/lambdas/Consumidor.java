14c05b60321c43ed25db45091f541061088e2131
Flavio Lima <flaviolima.s@live.com>
2021-03-22 22:46:44 -0300

    Implementando versoes de forEach

--- exercicios/src/classe/lambdas/Consumidor.java
+++ exercicios/src/classe/lambdas/Consumidor.java
@@ -7,17 +7,19 @@ import java.util.function.Consumer;
 public class Consumidor {
     // Definição de um T que não retorna nada
     public static void main(String[] args) {
-        Consumer<Produto> imprimir = p -> System.out.println(p.nome + "!!!");
+        Consumer<Produto> imprimirNome = p -> System.out.println(p.nome + "!!!");
 
         Produto produto1 = new Produto("Caneta", 12.34, .09);
-        imprimir.accept(produto1);
-        
+        imprimirNome.accept(produto1);
+
         Produto produto2 = new Produto("Notebook", 2987.99, .25);
         Produto produto3 = new Produto("Caderno", 19.90, .03);
         Produto produto4 = new Produto("Borracha", 7.80, .18);
         Produto produto5 = new Produto("Lapis", 4.39, .09);
 
         List<Produto> listProduto = Arrays.asList(produto1, produto2, produto3, produto4, produto5);
-        listProduto.forEach(imprimir);
+        listProduto.forEach(imprimirNome);
+        listProduto.forEach(p -> System.out.println(p.preco));
+        listProduto.forEach(System.out::println);
     }
 }
