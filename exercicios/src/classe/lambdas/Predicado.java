5062d53bf842e7a534b5494f3ff18c184af8528d
Flavio Lima <flaviolima.s@live.com>
2021-03-22 21:12:21 -0300

    alterando desconto aplicado ao produto

--- exercicios/src/classe/lambdas/Predicado.java
+++ exercicios/src/classe/lambdas/Predicado.java
@@ -6,7 +6,7 @@ public class Predicado {
     public static void main(String[] args) {
         Predicate<Produto> isCaro = prod -> (prod.preco * (1 - prod.desconto)) >= 750;
 
-        Produto produto = new Produto("Notebook", 3893.89, .15);
+        Produto produto = new Produto("Notebook", 3893.89, .89);
 
         System.out.println(isCaro.test(produto));
     }
