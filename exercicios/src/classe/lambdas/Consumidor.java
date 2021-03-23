b8ceb8303f4f63ece43eb7db48c4cbe8511a8d99
Flavio Lima <flaviolima.s@live.com>
2021-03-22 22:10:08 -0300

    Modelo da aula implementando Consumer para print pr

--- exercicios/src/classe/lambdas/Consumidor.java
+++ exercicios/src/classe/lambdas/Consumidor.java
@@ -7,13 +7,17 @@ import java.util.function.Consumer;
 public class Consumidor {
     // Definição de um T que não retorna nada
     public static void main(String[] args) {
-        
-        imprimeProduto(new Produto("Caneta", 12.34, .09));
-        
-        
-    }
-    public static void imprimeProduto(Produto produto) {
         Consumer<Produto> imprimir = p -> System.out.println(p.nome + "!!!");
-        imprimir.accept(produto);
+
+        Produto produto1 = new Produto("Caneta", 12.34, .09);
+        imprimir.accept(produto1);
+        
+        Produto produto2 = new Produto("Notebook", 2987.99, .25);
+        Produto produto3 = new Produto("Caderno", 19.90, .03);
+        Produto produto4 = new Produto("Borracha", 7.80, .18);
+        Produto produto5 = new Produto("Lapis", 4.39, .09);
+
+        List<Produto> listProduto = Arrays.asList(produto1, produto2, produto3, produto4, produto5);
+        listProduto.forEach(imprimir);
     }
 }
