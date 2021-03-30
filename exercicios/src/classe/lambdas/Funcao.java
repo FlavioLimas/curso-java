e15634f306af3d8a2dd6a8583306b916aec48791
Flavio Lima <flaviolima.s@live.com>
2021-03-29 21:25:36 -0300

    Alterando linha

--- exercicios/src/classe/lambdas/Funcao.java
+++ exercicios/src/classe/lambdas/Funcao.java
@@ -7,17 +7,17 @@ public class Funcao {
         Function<Integer, String> parOuImpar = 
             numero -> numero % 2 == 0 ? "Par" : "Ímpar";
     
-        System.out.println(parOuImpar.apply(32));
-
+            
         Function<String, String> oResultadoE = 
             valor -> "O resultado é: " + valor;
-        
+            
         Function<String, String> empolgado = valor -> valor + "!!!";
-
+            
         System.out.println(parOuImpar
             .andThen(oResultadoE)
             .andThen(empolgado)
             .apply(32));
+        System.out.println(parOuImpar.apply(32));
 
     }
     
