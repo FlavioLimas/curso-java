e7b5e9715610959a183ea48c963fe70a6270b742
Flavio Lima <flaviolima.s@live.com>
2021-03-29 22:52:45 -0300

    Possibilidade

--- exercicios/src/classe/lambdas/OperadorBinario.java
+++ exercicios/src/classe/lambdas/OperadorBinario.java
@@ -1,5 +1,6 @@
 package lambdas;
 
+import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 
 public class OperadorBinario {
@@ -7,6 +8,13 @@ public class OperadorBinario {
         
         BinaryOperator<Double> media = 
            (n1, n2) -> (n1 + n2) / 2;
-           System.out.println(media.apply(9.8, 5.7));
+
+        BiFunction<Double, Double, String> resultado = 
+        (n1, n2) -> ((n1 + n2) / 2) >= 7. ? "Aprovado" : "Reprovado";
+        System.out.println(
+            media
+            .apply(9.8, 5.7)
+        );
+
     }
 }
