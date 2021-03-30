2ae18b7a41060e9d0ebe3e92345bdbc65f392756
Flavio Lima <flaviolima.s@live.com>
2021-03-29 23:01:40 -0300

    2 params de entrada e 1 de saida

--- exercicios/src/classe/lambdas/OperadorBinario.java
+++ exercicios/src/classe/lambdas/OperadorBinario.java
@@ -9,12 +9,16 @@ public class OperadorBinario {
         BinaryOperator<Double> media = 
            (n1, n2) -> (n1 + n2) / 2;
 
-        BiFunction<Double, Double, String> resultado = 
-        (n1, n2) -> ((n1 + n2) / 2) >= 7. ? "Aprovado" : "Reprovado";
+        BiFunction<Double, Double, String> resultado =  (n1, n2) -> {
+            return ((n1 + n2) / 2) >= 7 ? "Aprovado" : "Reprovado";
+
+        };
+        
         System.out.println(
             media
             .apply(9.8, 5.7)
         );
-
+        
+        System.out.println(resultado.apply(9.7, 5.1));
     }
 }
