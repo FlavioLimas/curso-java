5208b94f6dbf6f749c7596ac8765a8e8d69428d3
Flavio Lima <flaviolima.s@live.com>
2021-03-30 22:45:25 -0300

    Definindo calculo preco com desconto

--- exercicios/src/classe/lambdas/Dasafio.java
+++ exercicios/src/classe/lambdas/Dasafio.java
@@ -1,8 +1,14 @@
 package lambdas;
 
+import java.util.function.BinaryOperator;
+
 public class Dasafio {
     public static void main(String[] args) {
         Produto p = new Produto("iPad", 3235.89, .13);
+
+        BinaryOperator<Double> precoReal = (preco, desconto) -> {
+            return (preco * (1 - desconto));
+        };
 
         
     }
