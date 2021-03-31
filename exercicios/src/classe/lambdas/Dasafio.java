1b27e3de6fc70c22bf12165b54ecf2f6e676405a
Flavio Lima <flaviolima.s@live.com>
2021-03-30 23:35:09 -0300

    correcao do desafio

--- exercicios/src/classe/lambdas/Dasafio.java
+++ exercicios/src/classe/lambdas/Dasafio.java
@@ -1,6 +1,5 @@
 package lambdas;
 
-import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
@@ -8,31 +7,28 @@ public class Dasafio {
     public static void main(String[] args) {
         Produto p = new Produto("iPad", 3235.89, .13);
 
-        BinaryOperator<Double> precoReal = (preco, desconto) -> {
-            return (preco * (1 - desconto));
-        };
+        Function<Produto, Double> precoFinal = 
+        (produto) -> (produto.preco * (1 - produto.desconto));
 
-        UnaryOperator<Double> impostoMunicipal = (preco) -> {
-            return (preco >= 2500 ? (preco * (1 + 8.5)) : 0);
-        };
+        UnaryOperator<Double> impostoMunicipal = 
+            (preco) -> (preco >= 2500 ? (preco * (1.085)) : preco);
 
-        UnaryOperator<Double> frete = (preco) -> {
-            return (preco >= 3000 ? preco + 100 : preco + 50);
-        };
-
-        Function<Double, Double> arredondar = 
-            valor -> Math.ceil(valor);
+        UnaryOperator<Double> frete = 
+            preco -> preco >= 3000 ? preco + 100 : preco + 50;
+        
+        UnaryOperator<Double> arredondar = 
+            preco -> Double.parseDouble(String.format("%.2f", preco));
 
         Function<Double, String> format =
-            valor -> String.format("%.2f", valor);
-            
+            preco -> ("R$" + preco).replace(".", ",");
+
         System.out.println(
-            precoReal
+            precoFinal
             .andThen(impostoMunicipal)
             .andThen(frete)
             .andThen(arredondar)
             .andThen(format)
-            .apply(p.preco, p.desconto)
+            .apply(p)
         );
     }
     
