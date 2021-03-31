1e4f60aec181b0e13e9fcce108b761c26ffbcef1
Flavio Lima <flaviolima.s@live.com>
2021-03-31 20:45:00 -0300

    Definindo impressao de objetos

---
+++ exercicios/src/classe/streams/ImprimindoObjetos.java
@@ -0,0 +1,13 @@
+package streams;
+
+import java.util.List;
+
+public class ImprimindoObjetos {
+    public static void main(String[] args) {
+        
+        List<String> aprovados = new Arrays.asList("Lu", "Gui", "Luca", "Ana");
+
+        
+
+    }
+}
