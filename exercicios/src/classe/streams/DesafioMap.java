c10b4a91c7c51389630fd9fa7f1243ca5896eed5
Flavio Lima <flaviolima.s@live.com>
2021-04-06 23:24:36 -0300

    Modelo da correcao com a chamada parseUnsignedInt

--- exercicios/src/classe/streams/DesafioMap.java
+++ exercicios/src/classe/streams/DesafioMap.java
@@ -2,7 +2,6 @@ package streams;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
@@ -10,19 +9,16 @@ public class DesafioMap {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
-        Consumer<String> println = System.out::println;
-        Function<Integer, String> binaryToString =
-            m -> Integer.toBinaryString(m);
-
         UnaryOperator<String> invertString =
-            m -> new StringBuilder(m).reverse().toString();
+            s -> new StringBuilder(s).reverse().toString();
 
-        Function<String, String> stringToBinary = 
-            m -> String.valueOf(Integer.parseUnsignedInt(m));
+        Function<String, Integer> binaryToString = 
+            s -> Integer.parseUnsignedInt(s, 2);
+        
         nums.stream()
-            .map(binaryToString)
+            .map(Integer::toBinaryString)
             .map(invertString)
-            .map(stringToBinary)
-            .forEach(println);
+            .map(binaryToString)
+            .forEach(System.out::println);
     }
 }
