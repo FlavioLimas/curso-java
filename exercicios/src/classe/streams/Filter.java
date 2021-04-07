b88f09c14b8dba01a231a5159584397ec5294180
Flavio Lima <flaviolima.s@live.com>
2021-04-06 23:44:41 -0300

    Definicao dos aunos

--- exercicios/src/classe/streams/Filter.java
+++ exercicios/src/classe/streams/Filter.java
@@ -1,7 +1,19 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Filter {
     public static void main(String[] args) {
         
+        List<Aluno> alunos =  Arrays.asList(
+            new Aluno("Ana", 7.8),
+            new Aluno("Bia", 5.8),
+            new Aluno("Daniel", 9.8),
+            new Aluno("Gui", 6.8),
+            new Aluno("Rebeca", 7.1),
+            new Aluno("Silvia", 8.8)
+        );
+
     }
 }
