92dd493d215d5f256e46a3aedfc061c3b0713b16
Flavio Lima <flaviolima.s@live.com>
2021-04-06 21:42:16 -0300

    Definindo consumer para impressao

--- exercicios/src/classe/streams/DesafioMap.java
+++ exercicios/src/classe/streams/DesafioMap.java
@@ -1,7 +1,13 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class DesafioMap {
     public static void main(String[] args) {
-        
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        Consumer<Object> pirnt = System.out::println;
     }
 }
