a0ff2e5b767dc50bcd557e5ffb3ef9092330e973
Flavio Lima <flaviolima.s@live.com>
2021-04-06 23:15:57 -0300

    Novo modelo

--- exercicios/src/classe/streams/DesafioMap.java
+++ exercicios/src/classe/streams/DesafioMap.java
@@ -3,19 +3,26 @@ package streams;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 public class DesafioMap {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
-        Consumer<String> print = System.out::print;
+        Consumer<String> println = System.out::println;
+        Function<Integer, String> binaryToString =
+            m -> Integer.toBinaryString(m);
+
         UnaryOperator<String> invertString =
-        m -> new StringBuilder(m).reverse().toString();
+            m -> new StringBuilder(m).reverse().toString();
+
+        Function<String, String> stringToBinary = 
+            m -> String.valueOf(Integer.parseUnsignedInt(m));
         nums.stream()
-            .map(m -> Integer.toBinaryString(m))
+            .map(binaryToString)
             .map(invertString)
-            .map(m -> String.valueOf(Integer.parseInt(m, 2)))
-            .forEach(print);
+            .map(stringToBinary)
+            .forEach(println);
     }
 }
