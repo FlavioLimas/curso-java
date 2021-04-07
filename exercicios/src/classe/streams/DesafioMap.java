4ce954af9184fdc070a96319e4af9d02e35ce1b5
Flavio Lima <flaviolima.s@live.com>
2021-04-06 22:44:37 -0300

    Implementando a conversao para binario

--- exercicios/src/classe/streams/DesafioMap.java
+++ exercicios/src/classe/streams/DesafioMap.java
@@ -3,11 +3,18 @@ package streams;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
 
 public class DesafioMap {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
-        Consumer<Object> pirnt = System.out::println;
+        Consumer<String> print = System.out::print;
+        // UnaryOperator<String> invertString =
+        // m -> new StringBuilder(m).reverse().toString();
+        nums.stream()
+            .map(m -> Integer.toBinaryString(m))
+            // .map(invertString)
+            .forEach(print);
     }
 }
