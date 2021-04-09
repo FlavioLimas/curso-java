66b6fab4e0c0d15aba7cad617e90380fe7fac844
Flavio Lima <flaviolima.s@live.com>
2021-04-08 21:57:39 -0300

    implementado desafio

--- exercicios/src/classe/streams/Canal.java
+++ exercicios/src/classe/streams/Canal.java
@@ -1,12 +1,16 @@
 package streams;
 
 public class Canal {
-    Integer id;
-    String nome;
-    public Canal(Integer id, String nome) {
+    private Integer id;
+    private String nome;
+    private Boolean ativo;
+
+    public Canal(Integer id, String nome, Boolean ativo) {
         this.id = id;
         this.nome = nome;
+        this.ativo = ativo;
     }
+
     public Integer getId() {
         return id;
     }
@@ -18,5 +22,13 @@ public class Canal {
     }
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
     }
 }
--- exercicios/src/classe/streams/TestCanal.java
+++ exercicios/src/classe/streams/TestCanal.java
@@ -1,8 +1,32 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class TestCanal {
     public static void main(String[] args) {
-                
+        List<Canal> canais = Arrays.asList(
+            new Canal(1, "GFA000012S", true),
+            new Canal(2, "PDI000013S", true),
+            new Canal(3, "GFA000014S", false),
+            new Canal(4, "GDE000015S", false),
+            new Canal(5, "GFA000016S", true)
+        );
+
+        Predicate<Canal> isGFA =
+            f -> f.getNome().contains("GFA");
+
+        Predicate<Canal> isAtivo = f -> f.getAtivo();
+
+        Function<Canal, String> printCanal = 
+            m -> "Id " +  m.getId() + " Canal de acesso " + m.getNome();
+            
+        canais.stream()
+            .filter(isGFA)
+            .filter(isAtivo)
+            .map(printCanal)
+            .forEach(System.out::println);
     }
-    
 }
