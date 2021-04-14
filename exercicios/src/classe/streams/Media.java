371a2c62f96fce4afa557cec1087828e66c7c842
Flavio Lima <flaviolima.s@live.com>
2021-04-13 21:07:47 -0300

    Definindo retorno de media

--- exercicios/src/classe/streams/Media.java
+++ exercicios/src/classe/streams/Media.java
@@ -1,5 +1,15 @@
 package streams;
 
 public class Media {
-    
+    private Double total;
+    private Integer contadorQuantidade;
+
+    public void adicionar(Double n) {
+        total += n;
+        contadorQuantidade++;
+    }
+
+    public Double getMedia() {
+        return total / contadorQuantidade;
+    }
 }
