7c86c13de45fa2c4ce3ee3ee762e4fdd892cf5c4
Flavio Lima <flaviolima.s@live.com>
2021-04-14 22:40:19 -0300

    implementando m2

--- exercicios/src/classe/streams/MediaTeste.java
+++ exercicios/src/classe/streams/MediaTeste.java
@@ -3,9 +3,11 @@ package streams;
 public class MediaTeste {
   public static void main(String[] args) {
     
-    Media n1 = new Media().adicionar(8.3).adicionar(6.7);
+    Media m1 = new Media().adicionar(8.3).adicionar(6.7);
+    Media m2 = new Media().adicionar(7.9).adicionar(6.6);
 
-    System.out.println(n1.getMedia());
+    System.out.println(m1.getMedia());
+    System.out.println(m2.getMedia());
   }
     
 }
