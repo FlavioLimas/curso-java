f493ea0e74f4ae52717d5741a41e3de13611f1ba
Flavio Lima <flaviolima.s@live.com>
2021-04-14 23:09:20 -0300

    virificando se algum item da lista nao foi aprovado

--- exercicios/src/classe/streams/Match.java
+++ exercicios/src/classe/streams/Match.java
@@ -1,7 +1,20 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Match {
     public static void main(String[] args) {
-        
+        List<Aluno> alunos = Arrays.asList(
+            new Aluno("Ana", 7.1),
+            new Aluno("Luna", 6.1),
+            new Aluno("Gui", 8.1),
+            new Aluno("Gabi", 10.)
+        );
+        Predicate<Aluno> isAprove = 
+            a -> a.nota >= 7.;
+
+        System.out.println(alunos.stream().allMatch(isAprove));
     }
 }
