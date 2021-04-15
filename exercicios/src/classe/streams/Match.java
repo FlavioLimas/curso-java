dc3c71492de341bf55ae780c7e006179048ccd2a
Flavio Lima <flaviolima.s@live.com>
2021-04-14 23:16:25 -0300

    Verificando aprovacao e reprovacao na lista

--- exercicios/src/classe/streams/Match.java
+++ exercicios/src/classe/streams/Match.java
@@ -8,14 +8,16 @@ public class Match {
     public static void main(String[] args) {
         List<Aluno> alunos = Arrays.asList(
             new Aluno("Ana", 7.1),
-            new Aluno("Luna", 6.1),
+            new Aluno("Luna", 8.5),
             new Aluno("Gui", 8.1),
             new Aluno("Gabi", 10.)
         );
-        Predicate<Aluno> isAprove = 
-            a -> a.nota >= 7.;
+        Predicate<Aluno> isAprove = a -> a.nota >= 7.;
+        Predicate<Aluno> isReprove = isAprove.negate();
 
         System.out.println(alunos.stream().allMatch(isAprove));
         System.out.println(alunos.stream().anyMatch(isAprove));
+        System.out.println(alunos.stream().noneMatch(isAprove));
+        System.out.println(alunos.stream().noneMatch(isReprove));
     }
 }
