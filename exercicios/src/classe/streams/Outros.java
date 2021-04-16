ee3e480a0defe1d1c0b4b9ebb255dcd03267ea7a
Flavio Lima <flaviolima.s@live.com>
2021-04-15 22:49:47 -0300

    Implementando distinct no list

--- exercicios/src/classe/streams/Outros.java
+++ exercicios/src/classe/streams/Outros.java
@@ -1,5 +1,24 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Outros {
-    
+    public static void main(String[] args) {
+        List<Aluno> alunos = Arrays.asList(
+        new Aluno("Ana", 7.1),
+        new Aluno("Luna", 8.5),
+        new Aluno("Gui", 8.1),
+        new Aluno("Gabi", 10.),
+        new Aluno("Ana", 7.1),
+        new Aluno("Pedro", 8.5),
+        new Aluno("Gui", 8.1),
+        new Aluno("Maria", 10.)
+    );
+
+    System.out.println("Distinct..." );
+    alunos.stream()
+    .distinct()
+    .forEach(System.out::println);
+    }
 }
