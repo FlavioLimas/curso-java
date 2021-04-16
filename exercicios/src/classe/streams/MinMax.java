c380a656e77dc9b6663d264ce641ad5d9b2fb247
Flavio Lima <flaviolima.s@live.com>
2021-04-15 21:34:57 -0300

    Menor nota sem mduar a chamada

--- exercicios/src/classe/streams/MinMax.java
+++ exercicios/src/classe/streams/MinMax.java
@@ -19,6 +19,11 @@ public class MinMax {
             if(a1.nota < a2.nota) return -1;
             return 0;
         };
+        Comparator<Aluno> piorNota = (a1, a2) -> {
+            if(a1.nota < a2.nota) return 1;
+            if(a1.nota > a2.nota) return -1;
+            return 0;
+        };
 
         System.out.println(
             alunos.stream()
@@ -27,6 +32,10 @@ public class MinMax {
         System.out.println(
             alunos.stream()
             .min(melhorNota).get()
+        );
+        System.out.println(
+            alunos.stream()
+            .max(piorNota).get()
         );
     }
     
