ecb10797cddd7f74be5fc4e3770bad71ef4bec57
Flavio Lima <flaviolima.s@live.com>
2021-04-19 21:16:10 -0300

    Iniciando novo capitulo tratamentoError

---
+++ exercicios/src/classe/excecao/Basico.java
@@ -0,0 +1,15 @@
+package excecao;
+
+import streams.Aluno;
+
+public class Basico {
+    public static void main(String[] args) {
+        System.out.println(7 / 0);
+
+        System.out.println("Fim :)");
+    }
+
+    public static void imprimirNomeAluno(Aluno aluno) {
+        System.out.println(aluno.nome);
+    }
+}
