f7e31acad2560829439887c7082a1c0a119f0436
Flavio Lima <flaviolima.s@live.com>
2021-04-19 21:19:52 -0300

    Copia de classe Aluno

--- exercicios/src/classe/excecao/Basico.java
+++ exercicios/src/classe/excecao/Basico.java
@@ -1,9 +1,9 @@
 package excecao;
 
-import streams.Aluno;
 
 public class Basico {
     public static void main(String[] args) {
+
         System.out.println(7 / 0);
 
         System.out.println("Fim :)");
