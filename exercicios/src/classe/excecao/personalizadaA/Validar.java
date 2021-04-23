d6f0d01aa008f9edba70800cb63e0fecf7f775a1
Flavio Lima <flaviolima.s@live.com>
2021-04-22 22:51:02 -0300

    Add validacao e renomeado metodo

--- exercicios/src/classe/excecao/personalizadaA/Validar.java
+++ exercicios/src/classe/excecao/personalizadaA/Validar.java
@@ -5,13 +5,18 @@ import excecao.Aluno;
 public class Validar {
     private Validar() {}
 
-    public static void aluno(Aluno aluno) {
+    public static void validarAluno(Aluno aluno) {
         if(aluno == null) {
             throw new IllegalArgumentException("O aluno está null!");
         }
 
         if (aluno.nome == null || aluno.nome.trim().isEmpty()) {
             throw new StringVaziaException("nome");
+        }
+
+        if (aluno.nota < 0 || aluno.nota > 10) {
+            throw new NumeroForaIntervaloException("nota");
+            
         }
     }
 }
