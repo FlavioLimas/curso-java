b0181ac5c3e2e16a2c42bd7d5aab4b7102070e91
Flavio Lima <flaviolima.s@live.com>
2021-04-22 23:08:32 -0300

    Add parametro na estancia

--- exercicios/src/classe/excecao/personalizadaA/TesteValidacoes.java
+++ exercicios/src/classe/excecao/personalizadaA/TesteValidacoes.java
@@ -5,7 +5,7 @@ import excecao.Aluno;
 public class TesteValidacoes {
     public static void main(String[] args) {
         try {
-            Aluno aluno = new Aluno("", -7.);
+            Aluno aluno = new Aluno("Ana", -7.);
             Validar.validarAluno(aluno);
         } catch (StringVaziaException e) {
             System.out.println(e.getMessage());
