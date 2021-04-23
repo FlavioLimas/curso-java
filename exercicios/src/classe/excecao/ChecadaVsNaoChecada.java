4a526dcdec29431f46c35007d57c7995068b049c
Flavio Lima <flaviolima.s@live.com>
2021-04-22 21:08:27 -0300

    modelo de lançamento de exception

--- exercicios/src/classe/excecao/ChecadaVsNaoChecada.java
+++ exercicios/src/classe/excecao/ChecadaVsNaoChecada.java
@@ -14,7 +14,7 @@ public class ChecadaVsNaoChecada {
     }
 
     /* 
-    * Exception NÃO chacada e NÃO verificada 
+    * Exception NÃO chacada e NÃO verificada
     */
     static void geraErro1() {
         throw new RuntimeException("Ocorreu um erro bem legal #01!");
@@ -23,7 +23,11 @@ public class ChecadaVsNaoChecada {
     /* 
     * Exception chacada e verificada
     */
-    static void geraErro2() throws Exception {
-        throw new Exception("Ocorreu um erro bem legal #02!");
+    static void geraErro2() {
+        try {
+            throw new Exception("Ocorreu um erro bem legal #02!");
+        } catch (Exception e) {
+            System.out.println("Que legal!");
+        }
     }
 }
