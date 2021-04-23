d3c059d6a0c6b2988ac1dde861497098618e110f
Flavio Lima <flaviolima.s@live.com>
2021-04-22 21:53:25 -0300

    move class

---
+++ exercicios/src/classe/excecao/personalizadaA/StringVaziaException.java
@@ -0,0 +1,14 @@
+package excecao.personalizadaA;
+
+public class StringVaziaException extends RuntimeException {
+    
+    private String nomeAtributo;
+
+    public StringVaziaException(String nomeAtributo) {
+        this.nomeAtributo = nomeAtributo;
+    }
+
+    public String getMessage() {
+        return String.format("O atributo %s está vazio ", nomeAtributo);
+    }
+}
