171bb6e642adf2199a20c0cdf5daa623b7fb2b5c
Flavio Lima <flaviolima.s@live.com>
2021-04-28 22:05:03 -0300

    Alterando a mensagem

--- exercicios/src/classe/excecao/Finally.java
+++ exercicios/src/classe/excecao/Finally.java
@@ -11,7 +11,7 @@ public class Finally {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
-            System.out.println("Finalmente...");
+            System.out.println("Finally...");
             entrada.close();
         }
         System.out.println("Fim!");
