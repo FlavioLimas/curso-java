601e27ff09f03883adac78d0944aff947eb8a021
Flavio Lima <flaviolima.s@live.com>
2021-04-28 22:01:19 -0300

    Teste entrada dividido por 7

--- exercicios/src/classe/excecao/Finally.java
+++ exercicios/src/classe/excecao/Finally.java
@@ -3,13 +3,17 @@ package excecao;
 import java.util.Scanner;
 
 public class Finally {
+    public static void main(String[] args) {
 
-    try {
-        Scanner entrada = new Scanner(System.in);
-        
-        sca
-    } catch (Exception e) {
+        try {
+            Scanner entrada = new Scanner(System.in);
 
+            System.out.println(7 / entrada.nextInt());
+
+            entrada.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Fim!");
     }
-    
 }
