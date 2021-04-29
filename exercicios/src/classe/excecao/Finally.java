05ea0fd77203a18bc6fd840cf718f7d356e2f0a5
Flavio Lima <flaviolima.s@live.com>
2021-04-28 21:56:30 -0300

    Definindo classe e try_catch

---
+++ exercicios/src/classe/excecao/Finally.java
@@ -0,0 +1,15 @@
+package excecao;
+
+import java.util.Scanner;
+
+public class Finally {
+
+    try {
+        Scanner entrada = new Scanner(System.in);
+        
+        sca
+    } catch (Exception e) {
+
+    }
+    
+}
