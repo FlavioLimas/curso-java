bc46b3b4d82907ea84f52db0835c8f3006a6dbc8
Flavio Lima <flaviolima.s@live.com>
2021-04-28 22:12:22 -0300

    definindo classe e motodos

---
+++ exercicios/src/classe/excecao/Causa.java
@@ -0,0 +1,15 @@
+package excecao;
+
+public class Causa {
+    public static void main(String[] args) {
+        
+    }
+
+    static void motodoA(Aluno aluno) {
+        
+    }
+    
+    static void motodoB(Aluno aluno) {
+
+    }
+}
